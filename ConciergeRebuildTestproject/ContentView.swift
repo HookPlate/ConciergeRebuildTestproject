35eee3a3cb7cc994bb3af79906a4198a33fb040e
HookPlate <41370249+HookPlate@users.noreply.github.com>
2023-04-23 10:43:26 +0100

    This adds a rough model to it, for now just initialised in contentview

--- ConciergeRebuildTestproject/ContentView.swift
+++ ConciergeRebuildTestproject/ContentView.swift
@@ -7,20 +7,37 @@
 
 import SwiftUI
 
+struct Session: Hashable {
+    var sessionTitle: String
+    var sessionTime: String
+    var sessionLeader: String
+    var numberOfAttendees: Int
+    var full: Bool
+}
+
+struct ScheduleForDay: Identifiable, Hashable {
+    var id: Int
+    var month: String
+    var day: String
+    var numberOfSessions: Int
+    var sessionsOnThatDay: [Session]
+}
+
 
 struct TaskRow: View {
+    var sessions: [Session]
     var body: some View {
         VStack(alignment: .leading, spacing: 5) {
-            Text("Skills getting started with iPad")
-            Text("2.00pm - 3.00pm")
+            Text(sessions[0].sessionTitle)
+            Text(sessions[0].sessionTime)
                 .foregroundColor(.secondary)
-            Text("Jim Heard")
+            Text(sessions[0].sessionLeader)
                 .foregroundColor(.secondary)
             HStack {
-                Text("6")
+                Text("\(sessions[0].numberOfAttendees)")
                     .fontWeight(.bold)
                 Spacer()
-                Text("Full")
+                Text(sessions[0].full ? "Full" : "")
                     .foregroundColor(.secondary)
             }
         }
@@ -28,20 +45,21 @@ struct TaskRow: View {
 }
 
 struct TitleRow: View {
+    var schedule: ScheduleForDay
     var body: some View {
         VStack(alignment: .leading, spacing: 2) {
             Spacer()
-            Text("Apr")
+            Text(schedule.month)
                 .foregroundColor(.secondary)
                 .fontWeight(.light)
             HStack {
-                Text("Thur")
+                Text(schedule.day)
                     .fontWeight(.bold)
-                Image(systemName: "15.circle.fill").foregroundColor(.blue)
+                Image(systemName: "\(schedule.id).circle.fill").foregroundColor(.blue)
                     .fontWeight(.bold)
                     .scaleEffect(1.3)
                 Spacer()
-                Text("2 Sessions")
+                Text("\(schedule.numberOfSessions)")
                     .foregroundColor(.secondary)
             }
         }
@@ -51,30 +69,43 @@ struct TitleRow: View {
 struct ContentView: View {
     @State private var isShowingCrew = false
     @State private var currentIndex = 0
+    var days: [ScheduleForDay]
+    var session = Session(sessionTitle: "Getting started on iPad", sessionTime: "10.00", sessionLeader: "Robin", numberOfAttendees: 3, full: false)
+    
+    init() {
+               let navBarAppearance = UINavigationBar.appearance()
+               navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.systemBlue]
+                navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.systemBlue]
+        days = [
+            ScheduleForDay(id: 13, month: "Jan", day: "Mon", numberOfSessions: 3, sessionsOnThatDay: [Session(sessionTitle: "Getting started on iPad", sessionTime: "10.00", sessionLeader: "Robin", numberOfAttendees: 3, full: false)]),
+            ScheduleForDay(id: 14, month: "Feb", day: "Tues", numberOfSessions: 5, sessionsOnThatDay: [Session(sessionTitle: "Getting started on iPad", sessionTime: "10.00", sessionLeader: "Robin", numberOfAttendees: 3, full: false)])
+        ]
+             }
+    
     var body: some View {
         NavigationStack {
             GeometryReader { geo in
                 ScrollViewReader { scrollView in
                     ScrollView(.horizontal) {
                         LazyHStack {
-                            ForEach(1...100, id: \.self) { index in
+                            ForEach(days, id: \.self) { day in
                                 List {
                                     Section {
-                                        TitleRow()
-                                        TaskRow()
-                                        TaskRow()
-                                        TaskRow()
+                                        TitleRow(schedule: day)
+                                        TaskRow(sessions: day.sessionsOnThatDay)
+//                                        TaskRow()
+//                                        TaskRow()
                                     }
                                 }
-                                .frame(width: geo.size.width - 50, height: geo.size.height - 50)
-                                .id(index)
+                                .frame(width: geo.size.width - 10, height: geo.size.height - 15)
+                                .id(day)
                                 .padding(5)
                             }
                         }
                     }
                     HStack {
                         Button(action: {
-                            withAnimation {
+                            withAnimation(.linear(duration: 2)) {
                                 currentIndex = (currentIndex == 0) ? currentIndex + 0 : currentIndex - 1
                                 scrollView.scrollTo(currentIndex,anchor: .leading)
                             }
