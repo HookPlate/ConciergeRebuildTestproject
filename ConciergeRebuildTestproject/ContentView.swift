524d6c182f69c80a922af89fb6a5d8cd022a047a
HookPlate <41370249+HookPlate@users.noreply.github.com>
2023-04-24 14:03:55 +0100

    More data to work with but the buttons are broken

--- ConciergeRebuildTestproject/ContentView.swift
+++ ConciergeRebuildTestproject/ContentView.swift
@@ -19,32 +19,34 @@ struct ScheduleForDay: Identifiable, Hashable {
     var id: Int
     var month: String
     var day: String
-    var numberOfSessions: Int
+    var numberOfSessions: Int {
+        sessionsOnThatDay.count
+    }
     var sessionsOnThatDay: [Session]
 }
 
 
-struct TaskRow: View {
-    var sessions: [Session]
+struct SessionRow: View {
+    var session: Session
     var body: some View {
         VStack(alignment: .leading, spacing: 5) {
-            Text(sessions[0].sessionTitle)
-            Text(sessions[0].sessionTime)
+            Text(session.sessionTitle)
+            Text(session.sessionTime)
                 .foregroundColor(.secondary)
-            Text(sessions[0].sessionLeader)
+            Text(session.sessionLeader)
                 .foregroundColor(.secondary)
             HStack {
-                Text("\(sessions[0].numberOfAttendees)")
+                Text("\(session.numberOfAttendees)")
                     .fontWeight(.bold)
                 Spacer()
-                Text(sessions[0].full ? "Full" : "")
+                Text(session.full ? "Full" : "")
                     .foregroundColor(.secondary)
             }
         }
     }
 }
 
-struct TitleRow: View {
+struct DayBannerRow: View {
     var schedule: ScheduleForDay
     var body: some View {
         VStack(alignment: .leading, spacing: 2) {
@@ -73,14 +75,79 @@ struct ContentView: View {
     var session = Session(sessionTitle: "Getting started on iPad", sessionTime: "10.00", sessionLeader: "Robin", numberOfAttendees: 3, full: false)
     
     init() {
-               let navBarAppearance = UINavigationBar.appearance()
-               navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.systemBlue]
-                navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.systemBlue]
+        let navBarAppearance = UINavigationBar.appearance()
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.systemBlue]
+        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.systemBlue]
         days = [
-            ScheduleForDay(id: 13, month: "Jan", day: "Mon", numberOfSessions: 3, sessionsOnThatDay: [Session(sessionTitle: "Getting started on iPad", sessionTime: "10.00", sessionLeader: "Robin", numberOfAttendees: 3, full: false)]),
-            ScheduleForDay(id: 14, month: "Feb", day: "Tues", numberOfSessions: 5, sessionsOnThatDay: [Session(sessionTitle: "Getting started on iPad", sessionTime: "10.00", sessionLeader: "Robin", numberOfAttendees: 3, full: false)])
+            ScheduleForDay(
+                id: 0,
+                month: "Jan",
+                day: "Mon",
+                sessionsOnThatDay:[
+                    Session(
+                        sessionTitle: "Getting started on iPad",
+                        sessionTime: "10.00",
+                        sessionLeader: "Robin",
+                        numberOfAttendees: 3,
+                        full: false),
+                    Session(
+                        sessionTitle: "Getting started on iPad",
+                        sessionTime: "10.00",
+                        sessionLeader: "Robin",
+                        numberOfAttendees: 3,
+                        full: false),
+                    Session(
+                        sessionTitle: "Getting started on iPad",
+                        sessionTime: "10.00",
+                        sessionLeader: "Robin",
+                        numberOfAttendees: 3,
+                        full: false),
+                    Session(
+                        sessionTitle: "Getting started on iPad",
+                        sessionTime: "10.00",
+                        sessionLeader: "Robin",
+                        numberOfAttendees: 3,
+                        full: false)
+                
+                ]),
+            ScheduleForDay(
+                id: 1,
+                month: "Feb",
+                day: "Tues",
+                sessionsOnThatDay: [
+                    Session(
+                        sessionTitle: "Getting started on iPhone",
+                        sessionTime: "10.00",
+                        sessionLeader: "Jim",
+                        numberOfAttendees: 4,
+                        full: false),
+                    Session(
+                        sessionTitle: "Getting started on iPhone",
+                        sessionTime: "10.00",
+                        sessionLeader: "Jim",
+                        numberOfAttendees: 4,
+                        full: false),
+                    Session(
+                        sessionTitle: "Getting started on iPhone",
+                        sessionTime: "10.00",
+                        sessionLeader: "Jim",
+                        numberOfAttendees: 4,
+                        full: false)
+                ]),
+            ScheduleForDay(
+                id: 2,
+                month: "Feb",
+                day: "Tues",
+                sessionsOnThatDay: [
+                    Session(
+                        sessionTitle: "Getting started on iPhone",
+                        sessionTime: "10.00",
+                        sessionLeader: "Jim",
+                        numberOfAttendees: 4,
+                        full: false)
+                ])
         ]
-             }
+    }
     
     var body: some View {
         NavigationStack {
@@ -90,12 +157,13 @@ struct ContentView: View {
                         LazyHStack {
                             ForEach(days, id: \.self) { day in
                                 List {
-                                    Section {
-                                        TitleRow(schedule: day)
-                                        TaskRow(sessions: day.sessionsOnThatDay)
-//                                        TaskRow()
-//                                        TaskRow()
+                                    DayBannerRow(schedule: day)
+                                    ForEach(day.sessionsOnThatDay, id: \.self) { session in
+                                        SessionRow(session: session)
                                     }
+                                    
+                    
+                                    
                                 }
                                 .frame(width: geo.size.width - 10, height: geo.size.height - 15)
                                 .id(day)
@@ -105,7 +173,7 @@ struct ContentView: View {
                     }
                     HStack {
                         Button(action: {
-                            withAnimation(.linear(duration: 2)) {
+                            withAnimation {
                                 currentIndex = (currentIndex == 0) ? currentIndex + 0 : currentIndex - 1
                                 scrollView.scrollTo(currentIndex,anchor: .leading)
                             }
@@ -117,6 +185,7 @@ struct ContentView: View {
                         Button(action: {
                             withAnimation {
                                 currentIndex = (currentIndex == 0) ? currentIndex + 2 : currentIndex + 1
+                                print("Scrolling forwards")
                                 scrollView.scrollTo(currentIndex,anchor: .leading)
                             }
                         }) {
