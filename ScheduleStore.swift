83e867419ed8b537c2d25676f6f7c40a79ffe806
HookPlate <41370249+HookPlate@users.noreply.github.com>
2023-04-25 12:58:22 +0100

    Adding the ability to create random data in the model, next is make sure the sessions don't repeat.

--- ScheduleStore.swift
+++ ScheduleStore.swift
@@ -56,25 +56,71 @@ class ScheduleStore: ObservableObject {
     ]
     
     init() {
+        var myRandonAmount = Int(arc4random_uniform(7))
         var myIndex = 0
-        for day in daysOfTheWeek {
-            for session in sessionTitles {
-                days.append(
-                    ScheduleForDay(
-                        id: myIndex,
-                        month: months.randomElement()!,
-                        day: day,
-                        sessionsOnThatDay:[
-                                Session(sessionTitle: session,
-                                        sessionTime: "2.00-3.00pm",
-                                        sessionLeader: trainers.randomElement()!,
-                                        numberOfAttendees: 4,
-                                        full: true)
-                        ]))
-                        myIndex += 1
-            }
+        for i in 0...9 {
+            sessions.append(
+                Session(
+                    sessionTitle: sessionTitles[myIndex],
+                    sessionTime: "2.00-3.00pm",
+                    sessionLeader: trainers.randomElement()!,
+                    numberOfAttendees: 4,
+                    full: true))
+            
+            myIndex += 1
+            myRandonAmount = Int(arc4random_uniform(7))
+//            for session in sessionTitles {
+//                days.append(
+//                    ScheduleForDay(
+//                        id: myIndex,
+//                        month: months.randomElement()!,
+//                        day: day,
+//                        sessionsOnThatDay:[
+//                                Session(sessionTitle: session,
+//                                        sessionTime: "2.00-3.00pm",
+//                                        sessionLeader: trainers.randomElement()!,
+//                                        numberOfAttendees: 4,
+//                                        full: true)
+//                        ]))
+//                        myIndex += 1
+//            }
         }
+        
+        for day in daysOfTheWeek {
+            days.append(
+                ScheduleForDay(
+                    id: Int(arc4random_uniform(7)),
+                    month: months[0],
+                    day: day,
+                    sessionsOnThatDay: returnRandomSessions(forThis: Int.random(in: 2..<5)))
+            )
+        }
+        
     }
+    
+    func returnRandomSessions(forThis: Int) -> [Session] {
+//        var resultSet = Set<Session>()
+//
+//        while resultSet.count < forThis {
+//            let randomIndex = Int(arc4random_uniform(UInt32(sessions.count)))
+//            resultSet.insert(sessions[randomIndex])
+//        }
+//
+//        let resultArray = Array(resultSet)
+//
+//        return resultArray
+        var resultArray = [Session]()
+        for _ in 0..<forThis {
+            let randomIndex = Int(arc4random_uniform(UInt32(sessions.count)))
+            resultArray.append(sessions[randomIndex])
+        }
+        return resultArray
+//        for i in 0...3 {
+//            let myRandomSession = sessions.randomElement()
+//        }
+    }
+    
+    var sessions: [Session] = []
     
     @Published var days: [ScheduleForDay] = []
 }
